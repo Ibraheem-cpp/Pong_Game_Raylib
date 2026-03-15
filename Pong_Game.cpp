8d5879a9d67da8dba49e640ce59344b1b8eda27b
Ibraheem-cpp <ibraheemtahir84@gmail.com>
2026-03-15 21:07:04 +0500

    Game Loop and Drawing Components

--- Pong_Game.cpp
+++ Pong_Game.cpp
@@ -5,8 +5,24 @@ using namespace std;
 
 int main()
 {
-    
+    const int height = 1280;
+    const int width = 720;
+    InitWindow(height, width, "Pong Game");
 
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+        DrawLine(height / 2, 0, height / 2, height, WHITE);
+        DrawCircle(height / 2, width / 2, 10.0, WHITE);
+        DrawRectangle(10, width / 2 - 50, 20, 100, WHITE);
+        DrawRectangle(height-30, width / 2 - 50, 20, 100, WHITE);
+
+
+        EndDrawing();
+    }
+
+
+    CloseWindow();
 
 
     return 0;
